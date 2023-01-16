7be0889a7a1dedd0dd07b7854ce0976fdb6a387b
sfjbs <sravankumarmunakala@gmail.com>
2023-01-16 05:44:21 +0000

    spring controller project done

---
+++ Day03/spring-controller/src/test/java/com/shravankumar/app/HelloControllerTest.java
@@ -0,0 +1,21 @@
+package com.shravankumar.app;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class HelloControllerTest {
+    HelloController helloController=new HelloController();
+    @Test
+    public void testSayHello(){
+        String expectedResult="Hello from App";
+        String result=helloController.SayHello();
+        Assert.assertEquals(expectedResult,result);
+    }
+    @Test
+    public void testrequestHello(){
+        String expectedResult="Hello from Request mapping";
+        String result=helloController.requestHello();
+        Assert.assertEquals(expectedResult,result);
+    }
+
+}
