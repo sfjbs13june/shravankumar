2b4a7a4de75d94c0b706d364b13e156954932e3f
sfjbs <sravankumarmunakala@gmail.com>
2023-01-17 14:06:35 +0000

    spring controller project done Day04

---
+++ Day04/spring-controller/README.md
@@ -0,0 +1,15 @@
+## Create Springboot project
+
+https://start.spring.io/
+
+## select maven version as 2.7.7 and java as 11 #Add Spring web dependency
+
+## Build project
+
+mvn clean install
+
+## Run  application
+
+mvn spring-boot:run
+
+## Test application
---
+++ Day04/spring-controller/src/test/java/com/shravankumar/app/HelloControllerTest.java
@@ -0,0 +1,35 @@
+package com.shravankumar.app;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class HelloControllerTest {
+    HelloController helloController=new HelloController();
+    @Test
+    public void testSayHello(){
+        String expectedResult="Hello from App";
+        String result=helloController.SayHello();
+        Assert.assertEquals(expectedResult,result);
+    }
+    @Test
+    public void testrequestHello(){
+        String expectedResult="Hello from Request mapping";
+        String result=helloController.requestHello();
+        Assert.assertEquals(expectedResult,result);
+    }
+    @Test
+    public void testrequestWelcome(){
+        String name="world";
+        String expectedResult="welcome to "+name;
+        String result=helloController.welcome(name);
+        Assert.assertEquals(expectedResult,result);
+    }
+    @Test
+    public void testrequestSendMessage(){
+        String message="world";
+        String expectedResult="send message "+message;
+        String result=helloController.sendMessage(message);
+        Assert.assertEquals(expectedResult,result);
+    }
+
+}
