4fd38ed37ea0417a296e0626956a630ce6b3a9f9
sfjbs <sravankumarmunakala@gmail.com>
2023-01-23 16:35:58 +0000

    patient-app Commited

--- Day06/patient-app/src/main/java/com/shravankumar/app/controller/PatientController.java
+++ Day06/patient-app/src/main/java/com/shravankumar/app/controller/PatientController.java
@@ -3,15 +3,19 @@ package com.shravankumar.app.controller;
 import com.shravankumar.app.model.Patient;
 import com.shravankumar.app.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PatientController {
+    @Value("${app.name}")
+    String name;
     @Autowired
     PatientService patientService;
 
     @PostMapping("/patient/save")
     public Patient savePatient(@RequestBody Patient patient){
+        System.out.println(name);
         System.out.println(patient);
         return patient;
     }
